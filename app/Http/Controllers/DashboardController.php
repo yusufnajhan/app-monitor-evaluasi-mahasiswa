77e9ecb3b701f28e6bbc7cecc5e1b3f277d83462
PinKevin <emeriokevin2320@gmail.com>
2023-10-23 23:33:05 +0700

    Menampilkan semester mahasiswa saat ini di dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -19,7 +19,13 @@ class DashboardController extends Controller
         } else if ($user->role === 'dosenWali') {
             return view('dosen-wali.dashboard');
         } else if ($user->role === 'mahasiswa') {
-            return view('mahasiswa.dashboard');
+            $mahasiswa = $user->mahasiswa;
+
+            $tahunTempuh = date('Y') - $mahasiswa->angkatan;
+            $semester = $tahunTempuh * 2;
+            $semester += date('m') >= 8 || date('m') == 1 ? '1' : 0;
+
+            return view('mahasiswa.dashboard', compact('semester'));
         }
     }
 }
