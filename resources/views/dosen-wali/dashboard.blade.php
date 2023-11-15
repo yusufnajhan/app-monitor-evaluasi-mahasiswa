c99264175278ee8e4713e214ece42356e3fb61ee
afraa <semarang@alizza-afra.local>
2023-11-15 12:13:23 +0700

    perbaiki template

--- resources/views/dosen-wali/dashboard.blade.php
+++ resources/views/dosen-wali/dashboard.blade.php
@@ -3,77 +3,87 @@
     <div class="content-wrapper">
         <!-- Content Header (Page header) -->
         <div class="content-header">
-            <div class="container-fluid">
+            <div class="container">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                    </div><!-- /.row -->
-                </div><!-- /.container-fluid -->
-            </div>
-            <!-- /.content-header -->
-
-            <!-- Main content -->
-            <section class="content">
-                <div class="container-fluid">
-                    <h1>Selamat datang,</h1>
-                    <h2>Dosen {{ auth()->user()->dosenWali->nama }}!</h2>
-                </div><!-- /.container-fluid -->
-                <div class="content">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <div class="card card-primary card-outline">
-                                    <div class="card-body">
-                                        <h5 class="text-center"><a href="dosen-wali/daftar-mahasiswa">Daftar Mahasiswa</a>
-                                        </h5>
-                                    </div>
-                                </div><!-- /.card -->
-                                <div class="card card-primary card-outline">
-                                    <div class="card-body">
-                                        <h5 class="text-center"><a href="dosen-wali/setujui-irs">Setujui IRS</a>
-                                        </h5>
-                                    </div>
-                                </div><!-- /.card -->
-                                <div class="card card-primary card-outline">
-                                    <div class="card-body">
-                                        <h5 class="text-center"><a href="dosen-wali/setujui-khs">Setujui KHS</a>
-                                        </h5>
-                                    </div>
-                                </div><!-- /.card -->
-                                <div class="card card-primary card-outline">
-                                    <div class="card-body">
-                                        <h5 class="text-center"><a href="dosen-wali/setujui-pkl">Setujui PKL</a>
-                                        </h5>
-                                    </div>
-                                </div><!-- /.card -->
-                                <div class="card card-primary card-outline">
-                                    <div class="card-body">
-                                        <h5 class="text-center"><a href="dosen-wali/setujui-skripsi">Setujui Skripsi</a>
-                                        </h5>
-                                    </div>
-                                </div><!-- /.card -->
+                        @php
+                            $dosen = auth()->user()->dosenWali;
+                        @endphp
+                        <h1>Selamat datang,</h1>
+                        <h1 class="m-0"> Dosen {{ $dosen->nama }}!</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Dashboard</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container -->
+        </div>
+        <!-- Main content -->                
+            <div class="content">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-6">
+                        <section class="content">
+                        <div class="container-fluid">
+                            <h2 class="text-center display-4">Search</h2>
+                            <div class="row">
+                                <div class="col-md-8 offset-md-2">
+                                    <form action="simple-results.html">
+                                        <div class="input-group input-group-lg">
+                                            <input type="search" class="form-control form-control-lg" id="search-input" placeholder="Masukkan nama mahasiswa..">
+                                            <div class="input-group-append">
+                                                <button type="submit" class="btn btn-lg btn-default">
+                                                    <i class="fa fa-search"></i>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
                         </div>
-                        <div class="form-group">
-                            <div class="input-group input-group-lg">
-                                <form action="" method="get">
-                                    <input type="search" class="form-control form-control-lg" id="search-input"
-                                        placeholder="Type your keywords here">
-                                    <div class="input-group-append">
-                                        <button type="submit" class="btn btn-lg btn-default">
-                                            <i class="fa fa-search"></i>
-                                        </button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                        <div class="search-result">
+                    </section>
+                    <div class="search-result">
+                    </div>
+                            <div class="card card-primary card-outline">
+                                <div class="card-body">
+                                    <h5 class="text-center"><a href="dosen-wali/daftar-mahasiswa">Daftar Mahasiswa</a>
+                                    </h5>
+                                </div>
+                            </div><!-- /.card -->
+                            <div class="card card-primary card-outline">
+                                <div class="card-body">
+                                    <h5 class="text-center"><a href="dosen-wali/setujui-irs">Setujui IRS</a>
+                                    </h5>
+                                </div>
+                            </div><!-- /.card -->
+                            <div class="card card-primary card-outline">
+                                <div class="card-body">
+                                    <h5 class="text-center"><a href="dosen-wali/setujui-khs">Setujui KHS</a>
+                                    </h5>
+                                </div>
+                            </div><!-- /.card -->
+                            <div class="card card-primary card-outline">
+                                <div class="card-body">
+                                    <h5 class="text-center"><a href="dosen-wali/setujui-pkl">Setujui PKL</a>
+                                    </h5>
+                                </div>
+                            </div><!-- /.card -->
+                            <div class="card card-primary card-outline">
+                                <div class="card-body">
+                                    <h5 class="text-center"><a href="dosen-wali/setujui-skripsi">Setujui Skripsi</a>
+                                    </h5>
+                                </div>
+                            </div><!-- /.card -->
                         </div>
                     </div>
                 </div>
-            </section>
-            <!-- /.content -->
-        </div>
+            </div>
+        <!-- /.content -->
     </div>
+
 @endsection
 
 @section('script')
