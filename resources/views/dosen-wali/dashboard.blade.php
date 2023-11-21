24fe43fe02f6ec205f5323712a62962ae99375fb
afraa <semarang@alizza-afra.local>
2023-11-21 10:12:42 +0700

    dashoard doswal

--- resources/views/dosen-wali/dashboard.blade.php
+++ resources/views/dosen-wali/dashboard.blade.php
@@ -21,68 +21,99 @@
                 </div><!-- /.row -->
             </div><!-- /.container -->
         </div>
-        <!-- Main content -->                
-            <div class="content">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-6">
-                        <section class="content">
-                        <div class="container-fluid">
-                            <h2 class="text-center display-4">Search</h2>
-                            <div class="row">
-                                <div class="col-md-8 offset-md-2">
-                                    <form action="simple-results.html">
-                                        <div class="input-group input-group-lg">
-                                            <input type="search" class="form-control form-control-lg" id="search-input" placeholder="Masukkan nama mahasiswa..">
-                                            <div class="input-group-append">
-                                                <button type="submit" class="btn btn-lg btn-default">
-                                                    <i class="fa fa-search"></i>
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-                    <div class="search-result">
+    
+        <!-- Main content --> 
+        <section class="content">
+            <div class="container">
+                @if ($errors->any())
+                    <div class="red-alert">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
                     </div>
-                            <div class="card card-primary card-outline">
-                                <div class="card-body">
-                                    <h5 class="text-center"><a href="dosen-wali/daftar-mahasiswa">Daftar Mahasiswa</a>
-                                    </h5>
-                                </div>
-                            </div><!-- /.card -->
-                            <div class="card card-primary card-outline">
-                                <div class="card-body">
-                                    <h5 class="text-center"><a href="dosen-wali/setujui-irs">Setujui IRS</a>
-                                    </h5>
-                                </div>
-                            </div><!-- /.card -->
-                            <div class="card card-primary card-outline">
-                                <div class="card-body">
-                                    <h5 class="text-center"><a href="dosen-wali/setujui-khs">Setujui KHS</a>
-                                    </h5>
-                                </div>
-                            </div><!-- /.card -->
-                            <div class="card card-primary card-outline">
-                                <div class="card-body">
-                                    <h5 class="text-center"><a href="dosen-wali/setujui-pkl">Setujui PKL</a>
-                                    </h5>
-                                </div>
-                            </div><!-- /.card -->
-                            <div class="card card-primary card-outline">
-                                <div class="card-body">
-                                    <h5 class="text-center"><a href="dosen-wali/setujui-skripsi">Setujui Skripsi</a>
-                                    </h5>
-                                </div>
-                            </div><!-- /.card -->
+                @endif
+                <div class="row">
+                <div class="col-lg-6">
+                    <div class="card card-primary card-outline">
+                        <div class="card-body">
+                            <h5 class="text-center"><a href="dosen-wali/daftar-mahasiswa">Daftar Mahasiswa</a>
+                            </h5>
                         </div>
-                    </div>
+                    </div><!-- /.card -->
+                    <div class="card card-primary card-outline">
+                        <div class="card-body">
+                            <h5 class="text-center"><a href="dosen-wali/setujui-irs">Setujui IRS</a>
+                            </h5>
+                        </div>
+                    </div><!-- /.card -->
+                     <div class="card card-primary card-outline">
+                        <div class="card-body">
+                            <h5 class="text-center"><a href="dosen-wali/setujui-khs">Setujui KHS</a>
+                            </h5>
+                        </div>
+                    </div><!-- /.card -->
                 </div>
-            </div>
-        <!-- /.content -->
-    </div>
+                <div class="col-lg-6">
+                    <div class="card card-primary card-outline">
+                        <div class="card-body">
+                            <h5 class="text-center"><a href="dosen-wali/setujui-pkl">Setujui PKL</a>
+                            </h5>
+                        </div>
+                    </div><!-- /.card -->
+                    <div class="card card-primary card-outline">
+                        <div class="card-body">
+                            <h5 class="text-center"><a href="dosen-wali/setujui-skripsi">Setujui Skripsi</a>
+                            </h5>
+                        </div>
+                    </div><!-- /.card -->
+                </div>
+                </div>
+                <div class="row">
+                <div class="col-12">  
+                    <div class="card">
+                        <div class="card-header">
+                            <h2 class="card-title"><b>Progress Mahasiswa</b></h2>
+                        </div><!-- /.card-header -->   
+                        <div class="card-body">
+                            <section class="content">
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <form action="simple-results.html">
+                                                <div class="input-group input-group-lg">
+                                                    <input type="search" class="form-control form-control-lg" id="search-input" placeholder="Masukkan nama mahasiswa..">
+                                                    <div class="input-group-append">
+                                                        <button type="submit" class="btn btn-lg btn-default">
+                                                            <i class="fa fa-search"></i>
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </section>
+                            <div class="search-result">
+                            </div><br>
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>NIM</th>
+                                        <th>Nama Mahasiswa</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                
+                                </tbody>
+                            </table><br>
+                            
+                            </div>
+                        </div><!-- /.card-body -->
+                    </section>
 
 @endsection
 
