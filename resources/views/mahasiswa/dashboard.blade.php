f34ba01e176151d4fc1ba6795f211577e16ab2e1
PinKevin <emeriokevin2320@gmail.com>
2023-12-03 18:40:41 +0700

    SRS-8 - Masukin kotak ke dasbor mahasiswa

--- resources/views/mahasiswa/dashboard.blade.php
+++ resources/views/mahasiswa/dashboard.blade.php
@@ -75,14 +75,50 @@
                         <div class="col-lg-6">
                             <div class="card card-primary card-outline">
                                 <div class="card-body">
-                                    <h5 class="text-center"><a
-                                            href="/mahasiswa/progres-skripsi/{{ $mahasiswa->nim }}">Progres
+                                    <h5 class="text-center">
+                                        <a href="/mahasiswa/progres-skripsi/{{ $mahasiswa->nim }}">
+                                            Progres
                                             Skripsi
-                                            Mahasiswa</a></h5>
+                                            Mahasiswa
+                                        </a>
+                                    </h5>
                                 </div>
                             </div><!-- /.card -->
                         </div>
                     @endif
+                    <div class="d-flex flex-wrap justify-content-center">
+                        @for ($i = 1; $i <= 14; $i++)
+                            <button type="button" class="btn btn-primary btn-lg mr-3" data-toggle="modal"
+                                data-target="#smt-{{ $i }}" style="width: 207px; height: 75px;"
+                                @if ($i <= $mahasiswa->hitungSemester()) enabled @else disabled @endif>
+                                Semester {{ $i }}
+                            </button>
+
+                            <div class="modal fade" id="smt-{{ $i }}" tabindex="-1"
+                                aria-labelledby="smt-{{ $i }}-modal" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="smt-{{ $i }}-modal">Data Akademik
+                                                Semester
+                                                {{ $i }} </h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            {{ $i }}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            @if ($i % 5 == 0)
+                    </div><br>
+                    <div class="d-flex flex-wrap justify-content-center">
+                        @endif
+                        @endfor
+                    </div><!-- /.card -->
                 </div>
             </div>
         </div>
